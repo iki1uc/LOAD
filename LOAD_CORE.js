fadb29ccde8af7f8757b00f69467f849dc00944a
iki1uc <entertrainer@web.de>
2026-08-02 11:49:20 +0200

    Update LOAD_CORE.js

--- LOAD_CORE.js
+++ LOAD_CORE.js
@@ -1,23 +1,14 @@
-// LOAD_CORE.js
-
-import { LOAD_MAP } from "./LOAD_MAP.js";
-
 export const LOAD_CORE = {
 
-  async fetch(room){
-    const file = LOAD_MAP[room];
-    if(!file) return {};
-
+  async fetch(file){
     if(file.endsWith(".raw")){
       const res = await fetch(file);
       return await res.text();
     }
-
     if(file.endsWith(".json")){
       const res = await fetch(file);
       return await res.json();
     }
-
     return {};
   }
 };
