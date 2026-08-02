bfd4b792d0541c91cbb7ec1fa1c49d0482a18274
iki1uc <entertrainer@web.de>
2026-08-02 11:50:04 +0200

    Update LOAD_PIPE.js

--- LOAD_PIPE.js
+++ LOAD_PIPE.js
@@ -1,12 +1,9 @@
-// LOAD_PIPE.js
-
 import { LOAD_CORE } from "./LOAD_CORE.js";
+import { LOAD_MAP } from "./LOAD_MAP.js";
 
-export async function LOAD_PIPE(room){
-  const data = await LOAD_CORE.fetch(room);
-
-  return {
-    room,
-    data
-  };
+export async function LOAD_PIPE(key){
+  const file = LOAD_MAP[key];
+  if(!file) return {};
+  const data = await LOAD_CORE.fetch(file);
+  return { key, data };
 }
